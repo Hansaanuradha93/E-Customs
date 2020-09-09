4a785d78fdb359801a2c5519540dfc09a734f11b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-09 12:44:15 +0530

    Login scene designed

--- E Customs/Scenes/Login/LoginVC.swift
+++ E Customs/Scenes/Login/LoginVC.swift
@@ -1,10 +1,46 @@
 import UIKit
 
 class LoginVC: UIViewController {
+    
+    fileprivate let emailTextField = ECTextField(padding: 16, placeholderText: "Enter email")
+    fileprivate let passwordTextField = ECTextField(padding: 16, placeholderText: "Enter password")
+    fileprivate let loginButton = ECButton(backgroundColor: UIColor.appColor(.lightGray), title: "Log In", titleColor: .gray, fontSize: 21)
+    fileprivate let gotoSignupButton = ECButton(backgroundColor: .white, title: "Go to sign up", titleColor: .black, fontSize: 18)
+    
+    fileprivate lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 18
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.barTintColor = UIColor.black
+        setupUI()
+    }
+    
+    
+    fileprivate func setupUI() {
         view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Log In"
+        
+        emailTextField.keyboardType = .emailAddress
+        emailTextField.autocorrectionType = .no
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.autocorrectionType = .no
+        loginButton.isEnabled = false
+        
+        emailTextField.setRoundedBorder(borderColor: .black, borderWidth: 1, radius: 2)
+        passwordTextField.setRoundedBorder(borderColor: .black, borderWidth: 1, radius: 2)
+        loginButton.setRoundedBorder(borderColor: .black, borderWidth: 1, radius: 2)
+        
+        view.addSubview(verticalStackView)
+        loginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        verticalStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 30, left: 20, bottom: 0, right: 20))
+        
+        view.addSubview(gotoSignupButton)
+        gotoSignupButton.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
     }
 }
