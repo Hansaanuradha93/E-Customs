5cb681b527336b9134501751f5eefb4054ecc156
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-09 12:06:39 +0530

    Login scene added to the app flow

---
+++ E Customs/Scenes/Login/LoginVC.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.barTintColor = UIColor.black
+        view.backgroundColor = .white
+    }
+}
--- E Customs/Scenes/Signup/SignupVC.swift
+++ E Customs/Scenes/Signup/SignupVC.swift
@@ -9,7 +9,7 @@ class SignupVC: UIViewController {
     fileprivate let emailTextField = ECTextField(padding: 16, placeholderText: "Enter email")
     fileprivate let passwordTextField = ECTextField(padding: 16, placeholderText: "Enter password")
     fileprivate let signupButton = ECButton(backgroundColor: UIColor.appColor(.lightGray), title: "Sign Up", titleColor: .gray, fontSize: 21)
-    
+    fileprivate let goToLoginButton = ECButton(backgroundColor: .white, title: "Go to login", titleColor: .black, fontSize: 18)
     
     fileprivate lazy var verticalStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [fullNameTextField, emailTextField, passwordTextField, signupButton])
@@ -75,6 +75,12 @@ extension SignupVC {
     @objc fileprivate func handleKeyboardShow(notification: Notification) {
         self.verticalStackView.transform = CGAffineTransform(translationX: 0, y: -10)
     }
+    
+    
+    @objc fileprivate func handleGoToLogin() {
+        let controller = LoginVC()
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
 }
 
 
@@ -122,6 +128,7 @@ extension SignupVC {
         emailTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
         passwordTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
         signupButton.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        goToLoginButton.addTarget(self, action: #selector(handleGoToLogin), for: .touchUpInside)
     }
     
     
@@ -145,5 +152,8 @@ extension SignupVC {
         view.addSubview(verticalStackView)
         signupButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         verticalStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 30, left: 20, bottom: 0, right: 20))
+        
+        view.addSubview(goToLoginButton)
+        goToLoginButton.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
     }
 }
