3353e5f248ce683ed874bd27682996bd5a154001
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-15 21:58:53 +0530

    all products feched on Home screen

--- E Customs/Scenes/Home/HomeVC.swift
+++ E Customs/Scenes/Home/HomeVC.swift
@@ -1,10 +1,42 @@
 import UIKit
+import Firebase
 
 class HomeVC: UIViewController {
 
+    // MARK: Properties
+    let viewModel = HomeVM()
+    fileprivate var listener: ListenerRegistration?
+    
+    
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.isHidden = true
+        setupUI()
+        fetchProducts()
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if isMovingFromParent { listener?.remove() }
+    }
+}
+
+
+// MARK: - Methods
+extension HomeVC {
+    
+    fileprivate func fetchProducts() {
+        listener = viewModel.fetchProducts { (status) in
+            if status {
+                print(self.viewModel.products)
+            }
+        }
+    }
+    
+    
+    fileprivate func setupUI() {
         view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
     }
 }
