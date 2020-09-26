1ecc5f3e9886338d511c8ba554f65979c7b614fd
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-26 21:59:55 +0530

    Shopping bag items fetched

--- E Customs/Scenes/Bag/BagVM.swift
+++ E Customs/Scenes/Bag/BagVM.swift
@@ -1,3 +1,34 @@
 import UIKit
+import Firebase
 
-class BagVM {}
+class BagVM {
+    
+    var items = [Item]()
+    
+    
+    func fetchItems(completion: @escaping (Bool) -> ()) -> ListenerRegistration? {
+        let currentUserId = Auth.auth().currentUser?.uid ?? ""
+        let reference = Firestore.firestore().collection("bag").document(currentUserId).collection("items")
+        
+        let listener = reference.addSnapshotListener { querySnapshot, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(false)
+                return
+            }
+            guard let documentChanges = querySnapshot?.documentChanges else {
+                completion(false)
+                return
+            }
+            for change in documentChanges {
+                if change.type == .added {
+                    print(change.document.data())
+                    let item = Item(dictionary: change.document.data())
+                    self.items.append(item)
+                }
+            }
+            completion(true)
+        }
+        return listener
+    }
+}
