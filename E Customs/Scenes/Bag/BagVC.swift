be9a195093da5a720ee3ab16020f436a5f521af2
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-26 20:29:25 +0530

    Table view cell designed in BagVC

--- E Customs/Scenes/Bag/BagVC.swift
+++ E Customs/Scenes/Bag/BagVC.swift
@@ -8,22 +8,46 @@ class BagVC: UITableViewController {
         
         tableView.separatorStyle = .none
         tableView.register(NumberOfItemsCell.self, forCellReuseIdentifier: NumberOfItemsCell.reuseID)
+        tableView.register(ItemCell.self, forCellReuseIdentifier: ItemCell.reuseID)
     }
     
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if section == 0 {
+            return 1
+        } else if section == 1 {
+            return 3
+        }
+        return 0
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: NumberOfItemsCell.reuseID, for: indexPath) as! NumberOfItemsCell
-        cell.backgroundColor = .red
-        return cell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: NumberOfItemsCell.reuseID, for: indexPath) as! NumberOfItemsCell
+            cell.set(count: 5)
+            return cell
+        } else if indexPath.section == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ItemCell.reuseID, for: indexPath) as! ItemCell
+            cell.set()
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 70
+        } else if indexPath.section == 1 {
+            return 145
+        }
+        return 0
     }
 }
--- E Customs/Scenes/Bag/Cells/NumberOfItemsCell.swift
+++ E Customs/Scenes/Bag/Cells/NumberOfItemsCell.swift
@@ -2,9 +2,12 @@ import UIKit
 
 class NumberOfItemsCell: UITableViewCell {
     
+    // MARK: Properties
     static let reuseID = "NumberOfItemsCell"
-
+    fileprivate let itemsCountLabel = ECSemiBoldLabel(textAlignment: .left, fontSize: 19)
     
+    
+    // MARK: Initializers
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
@@ -12,10 +15,25 @@ class NumberOfItemsCell: UITableViewCell {
     
     
     required init?(coder: NSCoder) { fatalError() }
+}
+
+
+// MARK: - Methods
+extension NumberOfItemsCell {
+    
+    func set(count: Int) {
+        if count > 1 {
+            itemsCountLabel.text = "\(count) ITEMS"
+        } else if count == 0 {
+            itemsCountLabel.text = "\(count) ITEM"
+        }
+    }
     
     
     fileprivate func setupUI() {
-        
+        let padding: CGFloat = 24
+        selectionStyle = .none
+        addSubview(itemsCountLabel)
+        itemsCountLabel.fillSuperview(padding: .init(top: padding, left: padding, bottom: padding, right: padding))
     }
-    
 }
