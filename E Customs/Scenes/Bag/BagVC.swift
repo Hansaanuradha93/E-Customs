a1cf3489a65072b81bf355b0997da099eaa65f3b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-26 19:52:32 +0530

    Basic table view configured in BagVC

--- E Customs/Scenes/Bag/BagVC.swift
+++ E Customs/Scenes/Bag/BagVC.swift
@@ -1,9 +1,29 @@
 import UIKit
 
-class BagVC: UIViewController {
+class BagVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        tableView.separatorStyle = .none
+        tableView.register(NumberOfItemsCell.self, forCellReuseIdentifier: NumberOfItemsCell.reuseID)
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NumberOfItemsCell.reuseID, for: indexPath) as! NumberOfItemsCell
+        cell.backgroundColor = .red
+        return cell
     }
 }
