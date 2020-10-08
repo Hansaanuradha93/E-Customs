2a052589e1104f093a969eecf53f23798c660889
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-08 16:37:44 +0530

    Code cleaned

--- E Customs/Scenes/Request Box/Request Details/RequestDetailsVM.swift
+++ E Customs/Scenes/Request Box/Request Details/RequestDetailsVM.swift
@@ -4,22 +4,4 @@ import Firebase
 class RequestDetailsVM {
     
     var bindableIsApproving = Bindable<Bool>()
-
-    
-//    func updateStatus(request: Request, completion: @escaping (Bool, String) -> ()) {
-//        guard let id = request.id else { return }
-//        self.bindableIsApproving.value = true
-//        let reference = Firestore.firestore().collection("requests").document(id)
-//
-//        let quntity = ["isApproved": true]
-//        reference.updateData(quntity) { error in
-//            self.bindableIsApproving.value = false
-//            if let error = error {
-//                print(error)
-//                completion(false, error.localizedDescription)
-//                return
-//            }
-//            completion(true, "Request approved successfully")
-//        }
-//    }
 }
