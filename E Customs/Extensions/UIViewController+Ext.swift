c47ada5d578edd9af9d0b8e651e49c39e401b338
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-12 09:42:03 +0530

    Alert ui bug fixed

--- E Customs/Extensions/UIViewController+Ext.swift
+++ E Customs/Extensions/UIViewController+Ext.swift
@@ -4,7 +4,7 @@ extension UIViewController {
     
     func presentAlert(title: String, message: String, buttonTitle: String, action: ((_ alertAction: UIAlertAction) -> Void)? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: title, style: .default, handler: action)
+        let action = UIAlertAction(title: buttonTitle, style: .default, handler: action)
         alert.addAction(action)
         self.present(alert, animated: true)
     }
