b61662a22adaa675e0f9eae8ab082f7a2a02886d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-13 10:37:36 +0530

    OrderList screen designed

---
+++ E Customs/Scenes/Profile/Cells/OrderCell.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+class OrderCell: UITableViewCell {
+    
+    // MARK: Properties
+    static let reuseID = "OrderCell"
+    
+    fileprivate let thumbnailImageView = ECImageView(contentMode: .scaleAspectFill)
+    fileprivate let orderNumberLabel = ECRegularLabel(text: "Order #", textAlignment: .left, fontSize: 15, numberOfLines: 2)
+    fileprivate let itemsCountLabel = ECRegularLabel(text: "ITEMS",textAlignment: .left, textColor: .gray, fontSize: 15, numberOfLines: 2)
+    fileprivate let priceLabel = ECRegularLabel(text: "$", textAlignment: .left, textColor: .gray, fontSize: 15, numberOfLines: 2)
+    fileprivate let separatorLine = UIView()
+
+    
+    // MARK: Initializers
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError() }
+}
+
+
+// MARK: - Methods
+extension OrderCell {
+    
+//    func set(request: Request, isLastRequest: Bool) {
+//        sneakerNameLabel.text = request.sneakerName ?? ""
+//        ideaDescriptionLabel.text = request.ideaDescription ?? ""
+//        thumbnailImageView.downloadImage(from: request.thumbnailUrl ?? "")
+//
+//        if isLastRequest {
+//            separatorLine.alpha = 0
+//        } else {
+//            separatorLine.alpha = 1
+//        }
+//    }
+    
+    
+    fileprivate func setupUI() {
+        selectionStyle = .none
+        
+        let paddingTop: CGFloat = 24
+        let dimensions: CGFloat = 102
+        
+        let stackView = UIStackView(arrangedSubviews: [orderNumberLabel, itemsCountLabel, priceLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.alignment = .leading
+        
+        separatorLine.backgroundColor = .lightGray
+        
+        contentView.addSubviews(thumbnailImageView, stackView, separatorLine)
+
+        thumbnailImageView.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: paddingTop, left: paddingTop, bottom: 0, right: 0), size: .init(width: dimensions, height: dimensions))
+        stackView.anchor(top: thumbnailImageView.topAnchor, leading: thumbnailImageView.trailingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: 0, left: paddingTop / 2, bottom: paddingTop, right: paddingTop))
+        separatorLine.anchor(top: thumbnailImageView.bottomAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: paddingTop, left: paddingTop, bottom: 0, right: paddingTop), size: .init(width: 0, height: 0.2))
+    }
+}
--- E Customs/Scenes/Profile/Order List/OrderListVC.swift
+++ E Customs/Scenes/Profile/Order List/OrderListVC.swift
@@ -1,12 +1,44 @@
 import UIKit
 
-class OrderListVC: UIViewController {
+class OrderListVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+}
+
+
+// MARK: - UITableView
+extension OrderListVC {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: OrderCell.reuseID, for: indexPath) as! OrderCell
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+}
+
+
+// MARK: - Methods
+extension OrderListVC {
+    
+    fileprivate func setupUI() {
         navigationController?.navigationBar.barTintColor = UIColor.white
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .white
         title = Strings.orders
+        
+        tableView.separatorStyle = .none
+        tableView.register(OrderCell.self, forCellReuseIdentifier: OrderCell.reuseID)
     }
 }
