b820f13dd9862154b9521458b15a2a42b8b2b7b3
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-13 11:34:01 +0530

    Order model added

---
+++ E Customs/Model/Order.swift
@@ -0,0 +1,27 @@
+import Firebase
+
+struct Order {
+    
+    // MARK: Properties
+    var orderId, uid, status, paymentMethod, shippingMethod, address, thumbnailUrl: String?
+    var subtotal, proccessingFees, total : Double?
+    var itemCount: Int?
+    var timestamp: Timestamp?
+    
+   
+    // MARK: Initializers
+    init(dictionary: [String : Any]) {
+        self.orderId = dictionary["orderId"] as? String
+        self.uid = dictionary["uid"] as? String
+        self.status = dictionary["status"] as? String
+        self.paymentMethod = dictionary["paymentMethod"] as? String
+        self.shippingMethod = dictionary["shippingMethod"] as? String
+        self.address = dictionary["address"] as? String
+        self.thumbnailUrl = dictionary["thumbnailUrl"] as? String
+        self.subtotal = dictionary["subtotal"] as? Double
+        self.proccessingFees = dictionary["proccessingFees"] as? Double
+        self.total = dictionary["total"] as? Double
+        self.itemCount = dictionary["itemCount"] as? Int
+        self.timestamp = dictionary["timestamp"] as? Timestamp
+    }
+}
