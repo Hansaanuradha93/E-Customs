0401112efac35b8958709928517c1f8f2974696f
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-13 11:33:47 +0530

    Orders fetched from Firestore

--- E Customs/Scenes/Profile/Order List/OrderListVM.swift
+++ E Customs/Scenes/Profile/Order List/OrderListVM.swift
@@ -1,3 +1,41 @@
 import UIKit
+import Firebase
 
-class OrderListVM {}
+class OrderListVM {
+    
+    // MARK: Properties
+    var orders = [Order]()
+}
+
+
+// MARK: - Methods
+extension OrderListVM {
+    
+    func fetchOrders(completion: @escaping (Bool) -> ()) -> ListenerRegistration? {
+        let uid = Auth.auth().currentUser?.uid ?? ""
+        let reference = Firestore.firestore().collection("orders")
+                
+        let listener = reference.whereField("uid", isEqualTo: uid).addSnapshotListener { (querySnapshot, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(false)
+                return
+            }
+            
+            guard let documentChanges = querySnapshot?.documentChanges else {
+                completion(false)
+                return
+            }
+            
+            for change in documentChanges {
+                if change.type == .added {
+                    let order = Order(dictionary: change.document.data())
+                    print(order)
+                    self.orders.append(order)
+                }
+            }
+            completion(true)
+        }
+        return listener
+    }
+}
