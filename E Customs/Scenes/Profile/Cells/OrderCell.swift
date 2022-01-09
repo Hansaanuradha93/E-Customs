3002cebe9fc193d8cd2319a09339833bf811052d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-01-09 19:29:04 +0530

    OrderCell code refactored

--- E Customs/Scenes/Profile/Cells/OrderCell.swift
+++ E Customs/Scenes/Profile/Cells/OrderCell.swift
@@ -16,7 +16,8 @@ class OrderCell: UITableViewCell {
     // MARK: Initializers
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupUI()
+        self.style()
+        self.layout()
     }
     
     
@@ -41,9 +42,14 @@ extension OrderCell {
 // MARK: - Private Methods
 private extension OrderCell {
     
-    func setupUI() {
+    func style() {
         selectionStyle = .none
-        
+
+        separatorLine.backgroundColor = .lightGray
+    }
+    
+    
+    func layout() {
         let paddingTop: CGFloat = 24
         let dimensions: CGFloat = 102
         
@@ -51,13 +57,13 @@ private extension OrderCell {
         stackView.axis = .vertical
         stackView.spacing = 8
         stackView.alignment = .leading
-        
-        separatorLine.backgroundColor = .lightGray
-        
+                
         contentView.addSubviews(thumbnailImageView, stackView, separatorLine)
 
         thumbnailImageView.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: paddingTop, left: paddingTop, bottom: 0, right: 0), size: .init(width: dimensions, height: dimensions))
+        
         stackView.anchor(top: thumbnailImageView.topAnchor, leading: thumbnailImageView.trailingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: 0, left: paddingTop / 2, bottom: paddingTop, right: paddingTop))
+        
         separatorLine.anchor(top: thumbnailImageView.bottomAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: paddingTop, left: paddingTop, bottom: 0, right: paddingTop), size: .init(width: 0, height: 0.2))
     }
 }
