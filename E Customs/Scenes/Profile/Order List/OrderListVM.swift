8a486f65b1724a8012eabb5c3c764cd16cc8d257
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-01-17 15:12:50 +0530

    APi calls documentations added and memory leaks fixed

--- E Customs/Scenes/Profile/Order List/OrderListVM.swift
+++ E Customs/Scenes/Profile/Order List/OrderListVM.swift
@@ -12,11 +12,16 @@ final class OrderListVM {
 // MARK: - Methods
 extension OrderListVM {
     
+    /// This fetches orders made by a customer from firestore
+    /// - Parameter completion: Returns the status of the API call
+    /// - Returns: Returns a firebase listener
     func fetchOrders(completion: @escaping (Bool) -> ()) -> ListenerRegistration? {
         let uid = Auth.auth().currentUser?.uid ?? ""
         let reference = Firestore.firestore().collection("orders")
                         
-        let listener = reference.whereField("uid", isEqualTo: uid).addSnapshotListener { (querySnapshot, error) in
+        let listener = reference.whereField("uid", isEqualTo: uid).addSnapshotListener { [weak self] querySnapshot, error in
+            guard let self = self else { return }
+            
             if let error = error {
                 print(error.localizedDescription)
                 completion(false)
@@ -42,18 +47,24 @@ extension OrderListVM {
                     self.ordersDictionary.removeValue(forKey: order.orderId ?? "")
                 }
             }
+            
             self.sortOrdersByTimestamp(completion: completion)
         }
+        
         return listener
     }
     
     
+    /// This sort the orders by descending order
+    /// - Parameter completion: Returns the status of the sorting process
     private func sortOrdersByTimestamp(completion: @escaping (Bool) -> ()) {
         let values = Array(ordersDictionary.values)
+        
         orders = values.sorted(by: { (order1, order2) -> Bool in
             guard let timestamp1 = order1.timestamp, let timestamp2 = order2.timestamp else { return false }
             return timestamp1.compare(timestamp2) == .orderedDescending
         })
+        
         completion(true)
     }
 }
